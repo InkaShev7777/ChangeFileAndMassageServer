9f3894ddcd083e8e9a3c624c2aba0a65bb0c63f8
InkaShev7777 <ilyaschevchenko@ILYASCHEVCH1F8A>
2022-06-28 18:34:29 +0300

    add pr client

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChangeFileAndMassageServer
+namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
